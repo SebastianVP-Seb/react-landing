68b80d812edba96433cd582705dff6d8f814178b
SebastianVP-Seb <palacios.sebastian.24@outlook.com>
2022-02-03 22:40:22 -0600

    Primeros componentes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Header from './components/header/background/Header';
+import Ganar from './components/ganar/Ganar';
+import Requisitos from './components/requisitos/Requisistos';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Header />
+      <Ganar />
+      <Requisitos />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
